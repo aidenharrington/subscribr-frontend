610e124bdfea11be8a94eccbbf2aaf0386363f96
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2024-10-17 11:51:35 -0700

    added MUI theme and colors

---
+++ src/ui/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+        main: '#4997D0', // Baby blue
+        },
+        secondary: {
+        main: '#333333', // Grey
+        },
+    }
+});
+
+export default theme;
